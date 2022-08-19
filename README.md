1d364de474ad319bf804a630bc14104e897a1e32
kelvee21 <kelvee21@gmail.com>
2022-08-19 14:10:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-nextjs-zxto3n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-nextjs-zxto3n)
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <button className="btn">Hello daisyUI</button>
+      <div>Login </div>
+    </div>
+  );
+}
